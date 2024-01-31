361806bcab5dcb1b209c9b9c977383809c276403
GalaxyBalaxy <158230793+GalaxyBalaxy@users.noreply.github.com>
2024-01-30 23:14:20 -0600

    Verion Fix

--- EasyMusicPaste-Setup/setup.py
+++ EasyMusicPaste-Setup/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='easy_music_paste',
-    version='1',
+    version='1.1',
     packages=find_packages(),
     entry_points={
         'console_scripts': ['EMP=easy_music_paste.main:main']
